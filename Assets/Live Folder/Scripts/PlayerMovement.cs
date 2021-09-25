5e497fc65ffae6f05b41075485c4276207b1c9a6
Caina Correa Caldas <cainacc@hotmail.com>
2021-09-25 15:49:34 -0300

    A little refactor in progress

--- Assets/Live Folder/Scripts/PlayerMovement.cs
+++ Assets/Live Folder/Scripts/PlayerMovement.cs
@@ -1,10 +1,9 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    enum JumpState
+    private enum JumpState
     {
         NOT_JUMPING,
         BEFORE_RELEASING_JUMP_BUTTON,
@@ -12,36 +11,32 @@ public class PlayerMovement : MonoBehaviour
         STARTING_TRIGGER_JUMP,
     }
 
+    [Header("Movement")]
     [SerializeField]
     private Rigidbody2D rb2d = null;
-
-    [SerializeField]
-    private float horizontalSpeed = 0.0f;
-
-    [SerializeField]
-    private float horizontalDrag = 0.0f;
-
-    [SerializeField]
-    private float initialJumpSpeed = 0.0f;
-
     [SerializeField]
     private Transform bottomRaycastOrigin = null;
 
     [SerializeField]
-    private SpriteRenderer spriteRenderer = null;
-
-    [SerializeField]
-    private Animator playerAnimator = null;
-
-    [SerializeField]
     private Transform flipXObject = null;
+    [SerializeField]
+    private float horizontalSpeed = 0.0f;
+    [SerializeField]
+    private float horizontalDrag = 0.0f;
+    [SerializeField]
+    private float initialJumpSpeed = 0.0f;
 
+    private bool isOnGround;
+    private bool shouldJump;
+    private JumpState jumpState;
+    private List<Collider2D> walls = new List<Collider2D>();
+
+    [Header("Shooting")]
     [SerializeField]
     private Transform arrowSpawner = null;
-
     [SerializeField]
     private Transform arrowPrefab = null;
-
+    
     [SerializeField]
     private float maxChargingAttackImpulse = 0.0f;
     [SerializeField]
@@ -49,21 +44,22 @@ public class PlayerMovement : MonoBehaviour
 
     private float currentCharge;
 
-    private float horizontalInput;
+    [Header("Animations")]
+    [SerializeField]
+    private SpriteRenderer spriteRenderer = null;
+    [SerializeField]
+    private Animator playerAnimator = null;
 
+    // Input
+    private float horizontalInput;
     private bool isPressingShootInput;
     private bool isPressingJumpInput;
     private bool shouldDoTriggerJump;
 
-    private bool shouldJump;
-    private bool isOnGround;
-
-    private JumpState jumpState;
-
-    private List<Collider2D> walls = new List<Collider2D>();
-
     private void Awake()
     {
+        // NOTE: this shouln't be in this script. When some other script like GameManager or similar is created
+        // please take the next line to that new script.
         Application.targetFrameRate = 30;
     }
 
@@ -76,12 +72,7 @@ public class PlayerMovement : MonoBehaviour
 
         this.isPressingShootInput = Input.GetButton("Fire1");
 
-        // Animations
-        playerAnimator.SetFloat("horizontal", horizontalInput);
-        playerAnimator.SetBool("isOnGround", isOnGround);
-        playerAnimator.SetBool("Fire1", isPressingShootInput);
-
-        // Shoot
+        // Shooting
         if (Input.GetButtonUp("Fire1"))
         {
             Instantiate(arrowPrefab, arrowSpawner.position, arrowSpawner.rotation)
@@ -89,148 +80,150 @@ public class PlayerMovement : MonoBehaviour
 
             currentCharge = 0.0f;
         }
+
+        // Animations
+        playerAnimator.SetFloat("horizontal", horizontalInput);
+        playerAnimator.SetBool("isOnGround", isOnGround);
+        playerAnimator.SetBool("Fire1", isPressingShootInput);
     }
 
     private void FixedUpdate()
     {
-        // Horizontal
+        // Movement
         {
-            bool canIGoRight = true;
-            foreach (Collider2D wall in walls)
+            // Horizontal Movement
             {
-                canIGoRight = canIGoRight && wall.transform.position.x < transform.position.x;
+                bool canIGoRight = true;
+                foreach (Collider2D wall in walls)
+                {
+                    canIGoRight = canIGoRight && wall.transform.position.x < transform.position.x;
+                }
+
+                bool canIGoLeft = true;
+                foreach (Collider2D wall in walls)
+                {
+                    canIGoLeft = canIGoLeft && wall.transform.position.x > transform.position.x;
+                }
+
+                if ( (!isPressingShootInput || !isOnGround) && (horizontalInput > 0.0f && canIGoRight || horizontalInput < 0.0f && canIGoLeft))
+                {
+                    rb2d.AddForce(horizontalInput * horizontalSpeed * Vector2.right);
+                }
+
+                // Flip X Object (Rotation)
+                Vector3 anglesForFlipXObject = flipXObject.transform.eulerAngles;
+                if (horizontalInput > 0.1f)
+                {
+                    anglesForFlipXObject.y = 0.0f;
+                }
+                else if (horizontalInput < -0.1f)
+                {
+                    anglesForFlipXObject.y = 180.0f;
+                }
+                flipXObject.transform.eulerAngles = anglesForFlipXObject;
+
+                // Drag
+                rb2d.AddForce( horizontalDrag * rb2d.velocity.x * - Vector2.right);
             }
 
-            bool canIGoLeft = true;
-            foreach (Collider2D wall in walls)
+            // Is on ground
             {
-                canIGoLeft = canIGoLeft && wall.transform.position.x > transform.position.x;
+                this.isOnGround = Physics2D.Raycast(bottomRaycastOrigin.transform.position, -Vector2.up, distance: 0.1f, 
+                                   layerMask: (LayerMask.NameToLayer("Player") | LayerMask.NameToLayer("JumpTrigger")));
             }
 
-            if ( (!isPressingShootInput || !isOnGround) && (horizontalInput > 0.0f && canIGoRight || horizontalInput < 0.0f && canIGoLeft))
+            // Jumping
             {
-                rb2d.AddForce(horizontalInput * horizontalSpeed * Vector2.right);
-            }
+                // Output Logic
+                // NOTE: We are writting on rb2d on every fixed frame. That's not optimized, but let solve this later so
+                // code is more organized now.
+                switch (jumpState)
+                {
+                    case JumpState.NOT_JUMPING:
+                        rb2d.gravityScale = 5.0f;
+                        break;
+                    case JumpState.BEFORE_RELEASING_JUMP_BUTTON:
+                        rb2d.gravityScale = 2.0f;
+                        break;
+                    case JumpState.AFTER_RELEASING_JUMP_BUTTON:
+                        rb2d.gravityScale = 5.0f;
+                        break;
+                    case JumpState.STARTING_TRIGGER_JUMP:
+                        rb2d.gravityScale = 3.5f;
+                        break;
+                }
 
-            // Flip X Object (Rotation)
-            Vector3 anglesForFlipXObject = flipXObject.transform.eulerAngles;
-            if (horizontalInput > 0.1f)
-            {
-                anglesForFlipXObject.y = 0.0f;
-            }
-            else if (horizontalInput < -0.1f)
-            {
-                anglesForFlipXObject.y = 180.0f;
-            }
-            flipXObject.transform.eulerAngles = anglesForFlipXObject;
-
-            // Drag
-            rb2d.AddForce( horizontalDrag * rb2d.velocity.x * - Vector2.right);
-        }
-
-        // Is on ground
-        {
-            // this.isOnGround = Physics2D.Raycast(bottomRaycastOrigin.transform.position, -Vector2.up, distance: 0.1f,
-            //                     layerMask: (LayerMask.NameToLayer("Player")));
-
-            this.isOnGround = Physics2D.Raycast(bottomRaycastOrigin.transform.position, -Vector2.up, distance: 0.1f, 
-                               layerMask: (LayerMask.NameToLayer("Player") | LayerMask.NameToLayer("JumpTrigger")));
-            
-            /* PAINT THE PLAYER RED IF IT IS ON GROUND
-            this.isOnGround = Physics2D.BoxCast(origin: bottomRaycastOrigin.transform.position, size: new Vector2(0.35f, 0.2f), 
-                angle: 0.0f, direction: - Vector2.up, distance: 0.1f, layerMask: LayerMask.NameToLayer("Player"));
-            if (isOnGround)
-            {
-                spriteRenderer.color = Color.red;
-            }
-            else
-            {
-                spriteRenderer.color = Color.white;
-            }
-            */
-        }
-
-        // Jump
-        {
-            // Output Logic
-            switch (jumpState)
-            {
-                case JumpState.NOT_JUMPING:
-                    rb2d.gravityScale = 5.0f;
-                    // spriteRenderer.color = Color.white;
-                    break;
-                case JumpState.BEFORE_RELEASING_JUMP_BUTTON:
-                    rb2d.gravityScale = 2.0f;
-                    // spriteRenderer.color = Color.red;
-                    break;
-                case JumpState.AFTER_RELEASING_JUMP_BUTTON:
-                    rb2d.gravityScale = 5.0f;
-                    // spriteRenderer.color = Color.blue;
-                    break;
-                case JumpState.STARTING_TRIGGER_JUMP:
-                    rb2d.gravityScale = 3.5f;
-                    // spriteRenderer.color = Color.yellow;
-                    break;
-            }
-
-            // Next State Logic and State Transition Actions
-            switch (jumpState) 
-            {
-                case JumpState.NOT_JUMPING:
-                    if (this.shouldJump)
-                    {
-                        this.shouldJump = false;
-                        this.jumpState = JumpState.BEFORE_RELEASING_JUMP_BUTTON;
-                        
-                        rb2d.AddForce(Vector2.up * initialJumpSpeed);
-                    }
-                    break;
-                case JumpState.BEFORE_RELEASING_JUMP_BUTTON:
-                    if (shouldDoTriggerJump)
-                    {
-                        this.jumpState = JumpState.STARTING_TRIGGER_JUMP;
-                        shouldDoTriggerJump = false;
-                    }
-                    else if (!this.isPressingJumpInput)
-                    {
-                        this.jumpState = JumpState.AFTER_RELEASING_JUMP_BUTTON;
-
-                        if (rb2d.velocity.y > 0.0f)
+                // Next State Logic and State Transition Actions
+                switch (jumpState) 
+                {
+                    case JumpState.NOT_JUMPING:
+                        if (this.shouldJump)
                         {
-                            rb2d.velocity = new Vector2(rb2d.velocity.x, rb2d.velocity.y * 0.5f);
+                            // Next State Logic
+                            this.jumpState = JumpState.BEFORE_RELEASING_JUMP_BUTTON;
+                        
+                            // State Transition Actions
+                            this.shouldJump = false;
+                            rb2d.AddForce(Vector2.up * initialJumpSpeed);
                         }
-                    }
-                    else if (isOnGround && rb2d.velocity.y > 0.0f)
-                    {
-                        this.jumpState = JumpState.NOT_JUMPING;
-                    }
-                    break;
-                case JumpState.AFTER_RELEASING_JUMP_BUTTON:
-                    if (shouldDoTriggerJump)
-                    {
-                        this.jumpState = JumpState.STARTING_TRIGGER_JUMP;
-                        shouldDoTriggerJump = false;
-                    }
-                    else if (this.isOnGround)
-                    {
-                        this.jumpState = JumpState.NOT_JUMPING;
-                    }
-                    break;
-                case JumpState.STARTING_TRIGGER_JUMP:
-                    if (this.isPressingJumpInput)
-                    {
-                        this.jumpState = JumpState.BEFORE_RELEASING_JUMP_BUTTON;
+                        break;
+                    case JumpState.BEFORE_RELEASING_JUMP_BUTTON:
+                        if (shouldDoTriggerJump)
+                        {
+                            // Next State Logic
+                            this.jumpState = JumpState.STARTING_TRIGGER_JUMP;
 
-                    }
-                    else if (this.isOnGround)
-                    {
-                        this.jumpState = JumpState.NOT_JUMPING;
-                    }
-                    break;
+                            // State Transition Actions
+                            shouldDoTriggerJump = false;
+                        }
+                        else if (!this.isPressingJumpInput)
+                        {
+                            // Next State Logic
+                            this.jumpState = JumpState.AFTER_RELEASING_JUMP_BUTTON;
+
+                            // State Transition Actions
+                            if (rb2d.velocity.y > 0.0f)
+                            {
+                                rb2d.velocity = new Vector2(rb2d.velocity.x, rb2d.velocity.y * 0.5f);
+                            }
+                        }
+                        else if (isOnGround && rb2d.velocity.y > 0.0f)
+                        {
+                            // Next State Logic
+                            this.jumpState = JumpState.NOT_JUMPING;
+                        }
+                        break;
+                    case JumpState.AFTER_RELEASING_JUMP_BUTTON:
+                        if (shouldDoTriggerJump)
+                        {
+                            // Next State Logic
+                            this.jumpState = JumpState.STARTING_TRIGGER_JUMP;
+
+                            // State Transition Actions
+                            shouldDoTriggerJump = false;
+                        }
+                        else if (this.isOnGround)
+                        {
+                            // Next State Logic
+                            this.jumpState = JumpState.NOT_JUMPING;
+                        }
+                        break;
+                    case JumpState.STARTING_TRIGGER_JUMP:
+                        // State Transition Actions
+                        if (this.isPressingJumpInput)
+                        {
+                            this.jumpState = JumpState.BEFORE_RELEASING_JUMP_BUTTON;
+                        }
+                        else if (this.isOnGround)
+                        {
+                            this.jumpState = JumpState.NOT_JUMPING;
+                        }
+                        break;
+                }
             }
         }
 
-        // Charging Attack
+        // Shooting
         {
             if (isPressingShootInput)
             {
@@ -242,6 +235,7 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    // Movement
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.tag == "wall")
@@ -249,7 +243,6 @@ public class PlayerMovement : MonoBehaviour
             walls.Add(col);
         }
     }
-
     private void OnTriggerExit2D(Collider2D col)
     {
         if (col.tag == "wall")
@@ -257,7 +250,6 @@ public class PlayerMovement : MonoBehaviour
             walls.Remove(col);
         }
     }
-
     public void ApplyJumpTriggerImpulse(Vector3 impulse)
     {
         shouldDoTriggerJump = true;
